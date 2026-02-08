ec83616ced8efcc93ea3335b8917b43ef2c13e6e
raoxiaojia <raoxiaojia@gmail.com>
2026-02-08 07:08:44 -0800

    coremark batch testing script

--- run.sh
+++
@@ -1,4 +0,0 @@
-set -ex
-./WasmCert-Coq/wasm_coq_interpreter.exe wasm-coremark/coremark-minimal.wasm -r run
-./spec/interpreter/wasm wasm-coremark/coremark-minimal.wasm -e '(invoke "run")'
-./spec/interpreter/wasm -isa wasm-coremark/coremark-minimal.wasm -e '(invoke "run")'
---
+++ run_coremark.sh
@@ -0,0 +1,27 @@
+set -ex
+echo "Coremark - Rocq"
+cd WasmCert-Coq
+dune build
+./wasm_coq_interpreter.exe ../wasm-coremark/coremark-minimal.wasm -r run
+
+cd ../spec/interpreter
+git checkout 547d565
+make
+echo "Coremark - reference interpreter"
+./wasm ../../wasm-coremark/coremark-minimal.wasm -e '(invoke "run")'
+echo "Coremark - Isabelle (non-monadic)"
+./wasm -isa ../../wasm-coremark/coremark-minimal.wasm -e '(invoke "run")'
+
+echo "Coremark - Isabelle (monadic)"
+git checkout ddeaadc
+make
+./wasm -isa ../../wasm-coremark/coremark-minimal.wasm -e '(invoke "run")'
+
+echo "wasmtime"
+cd ../../wasm-coremark-rs
+cargo build
+./target/release/bm wasmtime
+echo "wasmi"
+./target/release/bm wasmi
+
+echo "Done"
