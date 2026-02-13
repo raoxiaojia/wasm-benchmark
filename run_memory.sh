de58434f5d660cea2f4ca9d9a00ee390883d5c12
raoxiaojia <raoxiaojia@gmail.com>
2026-02-13 06:33:47 -0800

    new isabelle and rocq targets

--- run_memory.sh
+++ run_memory.sh
@@ -1,55 +1,108 @@
+#!/usr/bin/env bash
 set -ex
 
-echo "wasmtime"
-time wasmtime --invoke "memory_allocate" memory_allocate.wat
-time wasmtime --invoke "memory_fill" memory_fill.wat
-time wasmtime --invoke "memory_store_iterate" memory_store_iterate.wat
-time wasmtime --invoke "memory_grow" memory_grow.wat
-echo "wasmi-release"
-time ./wasmi/target/release/wasmi_cli --invoke "memory_allocate" memory_allocate.wat
-time ./wasmi/target/release/wasmi_cli --invoke "memory_fill" memory_fill.wat
-time ./wasmi/target/release/wasmi_cli --invoke "memory_store_iterate" memory_store_iterate.wat
-time ./wasmi/target/release/wasmi_cli --invoke "memory_grow" memory_grow.wat
-echo "wasmi-dev"
-cd wasmi
-cargo build
-time cargo run --bin wasmi_cli ../memory_allocate.wat --invoke "memory_allocate"
-time cargo run --bin wasmi_cli ../memory_fill.wat --invoke "memory_fill"
-time cargo run --bin wasmi_cli ../memory_store_iterate.wat --invoke "memory_store_iterate"
-time cargo run --bin wasmi_cli ../memory_grow.wat --invoke "memory_grow"
+run_wasmtime() {
+  echo "wasmtime"
+  time wasmtime --invoke "memory_allocate" memory_allocate.wat
+  time wasmtime --invoke "memory_fill" memory_fill.wat
+  time wasmtime --invoke "memory_store_iterate" memory_store_iterate.wat
+  time wasmtime --invoke "memory_grow" memory_grow.wat
+}
 
-echo "Rocq"
-cd ../WasmCert-Coq
-dune build
-time ./wasm_coq_interpreter.exe --text ../memory_allocate.wat -r "memory_allocate"
-time ./wasm_coq_interpreter.exe --text ../memory_fill.wat -r "memory_fill"
-time ./wasm_coq_interpreter.exe --text ../memory_store_iterate.wat -r "memory_store_iterate"
-time ./wasm_coq_interpreter.exe --text ../memory_grow.wat -r "memory_grow"
+run_wasmi_release() {
+  echo "wasmi-release"
+  time ./wasmi/target/release/wasmi_cli --invoke "memory_allocate" memory_allocate.wat
+  time ./wasmi/target/release/wasmi_cli --invoke "memory_fill" memory_fill.wat
+  time ./wasmi/target/release/wasmi_cli --invoke "memory_store_iterate" memory_store_iterate.wat
+  time ./wasmi/target/release/wasmi_cli --invoke "memory_grow" memory_grow.wat
+}
 
-cd ../spec/interpreter
-git checkout 547d565
-make
-echo "reference interpreter"
-time ./wasm ../../memory_allocate.wat -e '(invoke "memory_allocate")'
-time ./wasm ../../memory_fill.wat -e '(invoke "memory_fill")'
-time ./wasm ../../memory_store_iterate.wat -e '(invoke "memory_store_iterate")'
-time ./wasm ../../memory_grow.wat -e '(invoke "memory_grow")'
-echo "Isabelle (non-monadic)"
-time ./wasm -isa ../../memory_allocate.wat -e '(invoke "memory_allocate")'
-time ./wasm -isa ../../memory_fill.wat -e '(invoke "memory_fill")'
-time ./wasm -isa ../../memory_store_iterate.wat -e '(invoke "memory_store_iterate")'
-time ./wasm -isa ../../memory_grow.wat -e '(invoke "memory_grow")'
+run_wasmi_dev() {
+  echo "wasmi-dev"
+  cd wasmi
+  cargo build
+  time cargo run --bin wasmi_cli ../memory_allocate.wat --invoke "memory_allocate"
+  time cargo run --bin wasmi_cli ../memory_fill.wat --invoke "memory_fill"
+  time cargo run --bin wasmi_cli ../memory_store_iterate.wat --invoke "memory_store_iterate"
+  time cargo run --bin wasmi_cli ../memory_grow.wat --invoke "memory_grow"
+  cd ..
+}
 
-echo "Isabelle (monadic)"
-git checkout ddeaadc
-make
-time ./wasm -isa ../../memory_allocate.wat -e '(invoke "memory_allocate")'
-# Monadic isabelle only supports 1.0 features
-#time ./wasm -isa ../../memory_fill.wat -e '(invoke "memory_fill")'
-time ./wasm -isa ../../memory_store_iterate.wat -e '(invoke "memory_store_iterate")'
-time ./wasm -isa ../../memory_grow.wat -e '(invoke "memory_grow")'
+run_rocq() {
+  echo "Rocq"
+  cd WasmCert-Coq
+  dune build
+  time ./wasm_coq_interpreter.exe --text ../memory_allocate.wat -r "memory_allocate"
+  time ./wasm_coq_interpreter.exe --text ../memory_fill.wat -r "memory_fill"
+  time ./wasm_coq_interpreter.exe --text ../memory_store_iterate.wat -r "memory_store_iterate"
+  time ./wasm_coq_interpreter.exe --text ../memory_grow.wat -r "memory_grow"
+  cd ..
+}
 
-echo "cleaning up"
-git checkout master
+run_reference() {
+  echo "reference interpreter"
+  cd spec/interpreter
+  git checkout a160f21
+  make
+  time ./wasm ../../memory_allocate.wat -e '(invoke "memory_allocate")'
+  time ./wasm ../../memory_fill.wat -e '(invoke "memory_fill")'
+  time ./wasm ../../memory_store_iterate.wat -e '(invoke "memory_store_iterate")'
+  time ./wasm ../../memory_grow.wat -e '(invoke "memory_grow")'
+  cd ../..
+}
+
+run_isabelle_non_monadic() {
+  echo "Isabelle (non-monadic)"
+  cd spec/interpreter
+  git checkout a160f21
+  make
+  time ./wasm -isa ../../memory_allocate.wat -e '(invoke "memory_allocate")'
+  time ./wasm -isa ../../memory_fill.wat -e '(invoke "memory_fill")'
+  time ./wasm -isa ../../memory_store_iterate.wat -e '(invoke "memory_store_iterate")'
+  time ./wasm -isa ../../memory_grow.wat -e '(invoke "memory_grow")'
+  cd ../..
+}
+
+run_isabelle_monadic() {
+  echo "Isabelle (monadic)"
+  cd spec/interpreter
+  git checkout ddeaadc
+  make
+  time ./wasm -isa ../../memory_allocate.wat -e '(invoke "memory_allocate")'
+  time ./wasm -isa ../../memory_store_iterate.wat -e '(invoke "memory_store_iterate")'
+  time ./wasm -isa ../../memory_grow.wat -e '(invoke "memory_grow")'
+  git checkout master
+  cd ../..
+}
+
+TARGET="${1:-all}"
+
+case "$TARGET" in
+  all)
+    run_wasmtime
+    run_wasmi_release
+    run_wasmi_dev
+    run_rocq
+    run_reference
+    run_isabelle_non_monadic
+    run_isabelle_monadic
+    ;;
+  wasmtime) run_wasmtime ;;
+  wasmi-release) run_wasmi_release ;;
+  wasmi-dev) run_wasmi_dev ;;
+  rocq) run_rocq ;;
+  reference) run_reference ;;
+  isabelle-non-monadic) run_isabelle_non_monadic ;;
+  isabelle-monadic) run_isabelle_monadic ;;
+  *)
+    echo "Unknown target: $TARGET"
+    echo "Valid targets:"
+    echo "  all, wasmtime, wasmi-release, wasmi-dev, rocq,"
+    echo "  reference, isabelle-non-monadic, isabelle-monadic"
+    exit 1
+    ;;
+esac
 
 echo "Done"
+
+
